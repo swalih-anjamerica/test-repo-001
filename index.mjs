8b84a4a051cdcc029e4013d8ff5990613291b418
swalih-anjamerica <swalih@anjamerica.com>
2023-05-25 16:57:41 +0530

    s

--- index.mjs
+++ index.mjs
@@ -1,10 +1,55 @@
-export const handler = async (event) => {
-    console.log("Test action added");
-    console.log("New Value added")
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+console.log('triggered aws event');
+        
+import {S3Client, GetObjectCommand} from '@aws-sdk/client-s3';
+import { RekognitionClient, DetectModerationLabelsCommand, StartContentModerationCommand, GetContentModerationCommand } from "@aws-sdk/client-rekognition";
+
+
+const region="us-east-2";
+const s3 = new S3Client({region});
+const client = new RekognitionClient({ region });
+
+export const handler = async (event, context) => {
+  // getting data from the bucket
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  
+  try {
+    
+    // const res=await videoModeration(bucket, key)
+    const res=await imageModeration(bucket, key);
+    console.log(res);
+  } catch (error) {
+    // error handling.
+    console.log(error)
+  }
 };
+
+const imageModeration=async (bucket, key)=>{
+  const params = { Image: { S3Object: { Bucket: bucket, Name:  key }, }, MaxLabels: 10 }
+  const command = new DetectModerationLabelsCommand(params);
+  const res = await client.send(command);
+  return res;
+}
+const videoModeration=async(bucket, key)=>{
+  const params={
+    "Video": {
+        "S3Object": {
+            "Bucket": bucket,
+            "Name": key
+        }
+    },
+     "MinConfidence": 50,
+    "NotificationChannel": {
+        "SNSTopicArn": "arn:aws:sns:us-east-1:nnnnnnnnnn:topic",
+        "RoleArn": "arn:aws:iam::nnnnnnnnnn:role/roleopic"
+    },
+    "JobTag": "DetectingLabels"
+}
+  const command=new StartContentModerationCommand(params);
+  const res=await client.send(command)
+  
+  const params2={
+   "JobId": res.JobId,
+  }
+  return res;
+}
