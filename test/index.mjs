0670d25305e3e04411eedd77dab8dd1657376500
swalih-anjamerica <swalih@anjamerica.com>
2023-05-29 17:55:10 +0530

    s

--- test/index.mjs
+++ test/index.mjs
@@ -4,9 +4,7 @@ import { updateAladdinDb } from './utils/api.utils.mjs';
 export const handler = async (event, context) => {
     // getting data from the bucket
     const bucket = event.Records[0].s3.bucket.name;
-    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-
-    
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));    
     try {
         // const res=await videoModeration(bucket, key)
         const res = await detectImageModerationContent(bucket, key);
